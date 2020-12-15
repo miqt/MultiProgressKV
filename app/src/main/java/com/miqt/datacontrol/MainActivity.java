605cb959867eb2767b35e0c8941efdbb44d14f0b
miqt <miqtdev@163.com>
2020-12-15 15:48:53 +0800

    更新demo示例

--- app/src/main/java/com/miqt/datacontrol/MainActivity.java
+++ app/src/main/java/com/miqt/datacontrol/MainActivity.java
@@ -1,6 +1,9 @@
 package com.miqt.datacontrol;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DataControl control  = new DataControl(this, DataControl.SaveType.DB);
-        control.putString("321","321");
-        control.putString("321","qqq");
-        control.putString("321","bbb");
+        final EditText ed_key = findViewById(R.id.ed_key);
+        final EditText ed_value = findViewById(R.id.ed_value);
+        final DataControl control = new DataControl(this, DataControl.SaveType.DB);
+
+        Button button = findViewById(R.id.btn_commit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = ed_key.getText().toString();
+                String value = ed_value.getText().toString();
+                control.putString(key, value);
+            }
+        });
     }
 }
